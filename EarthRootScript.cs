bbb984196fbf5d300e3c0b0285b3cd5660d7cf8c
dibbik <119895965+dibbik@users.noreply.github.com>
2023-06-02 07:29:39 +0300

    Add files via upload

---
+++ EarthRootScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DigitalRuby.Earth
+{
+    public class EarthRootScript : MonoBehaviour
+    {
+        private void Update()
+        {
+            transform.Rotate(0.0f, 0.01f, 0.0f);
+
+        }
+    }
+}
